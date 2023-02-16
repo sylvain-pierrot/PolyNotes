fdac01fd67e58a180cdf13ed68e13f9bd6043b47
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2023-02-17 00:30:21 +0100

    feat: ci + test helm

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import TaskList from './components/TaskList';
+import WindowSize from './components/WindowSize'
+
+function App() {
+  return (
+    <div>
+      <h1>TodoList</h1>
+      <TaskList name="Todo" />
+      <TaskList name="WIP" />
+      <WindowSize />
+    </div>
+  );
+}
+
+export default App;
