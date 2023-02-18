ee8e9fa574a302eec410a9addba7a9506b19948d
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2023-02-18 14:20:28 +0100

    test ci/cd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import WindowSize from './components/WindowSize'
 function App() {
   return (
     <div>
-      <h1>TodoList</h1>
+      <h1>TodoList - test</h1>
       <TaskList name="Todo" />
       <TaskList name="WIP" />
       <WindowSize />
