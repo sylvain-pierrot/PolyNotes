ec7e3ca0c15864672b81750c264fc77e6ccf5921
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2023-03-01 14:09:47 +0100

    feat: droggable blocks

--- frontend/src/components/Page/Page.tsx
+++ frontend/src/components/Page/Page.tsx
@@ -1,133 +1,16 @@
-import React, { useState } from "react";
-import TextBlock from "../TextBlock/TextBlock";
 import "./Page.css";
-import { Tree } from "antd";
-import type { DataNode, TreeProps } from "antd/es/tree";
+import SortableBlocks from "../SortableBlocks/SortableBlocks";
+import { createRange } from "../../utils/array";
+import { useState } from "react";
 
-const defaultData: DataNode[] = [
-  {
-    title: (
-      <TextBlock
-        placeholder={"Untitiled"}
-        content={""}
-        className={"block page-block"}
-      />
-    ),
-    key: "0",
-  },
-  {
-    title: (
-      <TextBlock
-        placeholder={"Untitiled"}
-        content={""}
-        className={"block page-block"}
-      />
-    ),
-    key: "1",
-  },
-  {
-    title: (
-      <TextBlock
-        placeholder={"Untitiled"}
-        content={""}
-        className={"block page-block"}
-      />
-    ),
-    key: "2",
-  },
-];
+function getMockItems() {
+  return createRange(4, (index) => ({ id: index + 1 }));
+}
 
 const Page: React.FC = () => {
-  // const [title, setTtitle] = useState("");
+  const [blocks, setBlocks] = useState(getMockItems);
 
-  // const handleChange = (event: any) => {
-  //   setTtitle(event.target.value);
-  // };
-
-  const [gData, setGData] = useState(defaultData);
-
-  const onDragEnter: TreeProps["onDragEnter"] = (info) => {
-    console.log(info);
-    // expandedKeys 需要受控时设置
-    // setExpandedKeys(info.expandedKeys)
-  };
-
-  const onDrop: TreeProps["onDrop"] = (info) => {
-    console.log(info);
-    const dropKey = info.node.key;
-    const dragKey = info.dragNode.key;
-    const dropPos = info.node.pos.split("-");
-    const dropPosition =
-      info.dropPosition - Number(dropPos[dropPos.length - 1]);
-
-    const loop = (
-      data: DataNode[],
-      key: React.Key,
-      callback: (node: DataNode, i: number, data: DataNode[]) => void
-    ) => {
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].key === key) {
-          return callback(data[i], i, data);
-        }
-        if (data[i].children) {
-          loop(data[i].children!, key, callback);
-        }
-      }
-    };
-    const data = [...gData];
-
-    // Find dragObject
-    let dragObj: DataNode;
-    loop(data, dragKey, (item, index, arr) => {
-      arr.splice(index, 1);
-      dragObj = item;
-    });
-
-    let ar: DataNode[] = [];
-    let i: number;
-    loop(data, dropKey, (_item, index, arr) => {
-      ar = arr;
-      i = index;
-    });
-    if (dropPosition === -1) {
-      ar.splice(i!, 0, dragObj!);
-    } else {
-      ar.splice(i! + 1, 0, dragObj!);
-    }
-
-    setGData(data);
-  };
-
-  return (
-    <>
-      {/* <TextBlock
-        placeholder={"Untitiled"}
-        content={title}
-        className={"block page-block"}
-      />
-      <div className="page-core">
-        <TextBlock
-          placeholder={"Press 'space' for AI, '/' for commands..."}
-          content={""}
-          className={"block text-block"}
-        />
-        <TextBlock
-          placeholder={"Heading 1"}
-          content={""}
-          className={"block header-block"}
-        />
-      </div> */}
-      <Tree
-        className="draggable-tree"
-        draggable
-        blockNode={true}
-        onDragEnter={onDragEnter}
-        onDrop={onDrop}
-        treeData={gData}
-        checkStrictly={true}
-      />
-    </>
-  );
+  return <SortableBlocks blocks={blocks} onChange={setBlocks} />;
 };
 
 export default Page;
