420c291fba91e796f8af352013c51d0d76730c14
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2023-03-09 00:40:04 +0100

    refactor: remove drag and drop

--- frontend/src/components/Block/Block.tsx
+++ frontend/src/components/Block/Block.tsx
@@ -1,40 +1,22 @@
-import { UniqueIdentifier, useDraggable } from "@dnd-kit/core";
-import { HolderOutlined } from "@ant-design/icons";
+import { PlusOutlined } from "@ant-design/icons";
 import { Button } from "antd";
-import React, { useEffect } from "react";
+import React from "react";
 import "./Block.css";
-import Droppable from "./components/Droppable/Droppable";
-import TextBlock from "./components/TextBlock/TextBlock";
 
 interface IPropsBlock {
-  id: UniqueIdentifier;
-  dragOverlay?: boolean;
+  children?: JSX.Element;
 }
 
-const Block: React.FC<IPropsBlock> = ({ id, dragOverlay }) => {
-  const { attributes, listeners, setNodeRef } = useDraggable({
-    id: id,
-  });
-
-  const style = {
-    opacity: dragOverlay ? 0.5 : 1,
-  };
-
+const Block: React.FC<IPropsBlock> = ({ children }) => {
   return (
-    <div className="Block" ref={setNodeRef} {...attributes} style={style}>
+    <div className="Block">
       <Button
-        className="DragHandle"
-        {...listeners}
+        className="add-block"
         type="text"
-        icon={<HolderOutlined />}
+        icon={<PlusOutlined />}
+        size="middle"
       />
-
-      <TextBlock id={id} />
-
-      <Droppable id={`${id + "top"}`} position={"top"} blockId={id} />
-      {/* <Droppable id={`${id + "bottom"}`} position={"bottom"} blockId={id} /> */}
-      <Droppable id={`${id + "right"}`} position={"right"} blockId={id} />
-      {/* <Droppable id={`${id + "left"}`} position={"left"} blockId={id} /> */}
+      {children}
     </div>
   );
 };
