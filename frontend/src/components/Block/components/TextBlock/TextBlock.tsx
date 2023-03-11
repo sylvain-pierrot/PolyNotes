987cf9ca81312453e13229f7f3c976f442902335
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2023-03-11 20:33:22 +0100

    feat(frontend): switch block with up and down arrows

--- frontend/src/components/Block/components/TextBlock/TextBlock.tsx
+++ frontend/src/components/Block/components/TextBlock/TextBlock.tsx
@@ -3,11 +3,11 @@ import {
   EditorContent,
   useEditor,
   FloatingMenu,
-  ReactNodeViewRenderer,
+  Editor,
 } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Highlight from "@tiptap/extension-highlight";
-import React from "react";
+import React, { forwardRef, Ref, useImperativeHandle } from "react";
 import "./TextBlock.css";
 import { Avatar, Button, List } from "antd";
 import {
@@ -32,206 +32,220 @@ import { destroyBlock, newBlock } from "../../../../store/slices/blocksSlice";
 interface IPropsTextBlock {
   id: string;
   content: string;
-  type: string;
-  onChange: () => void;
+  type?: string;
+  onDestroy: () => void;
+  onArrowPressed?: (event: any) => void;
 }
 
-const TextBlock: React.FC<IPropsTextBlock> = ({
-  id,
-  content,
-  type,
-  onChange,
-}) => {
-  const ShortcutsExtension = Extension.create({
-    addKeyboardShortcuts() {
-      return {
-        Enter: () => {
-          handleEnter();
-          return true;
-        },
-        Backspace: ({ editor }) => {
-          if (editor.isEmpty) {
-            handleDestroyOnEmpty(id);
-          }
-          return false;
-        },
-      };
-    },
-  });
+const TextBlock = forwardRef(
+  (
+    { id, content, onDestroy, onArrowPressed }: IPropsTextBlock,
+    ref: Ref<Editor | null>
+  ) => {
+    // Extension
+    const ShortcutsExtension = Extension.create({
+      addKeyboardShortcuts() {
+        return {
+          Enter: () => {
+            handleEnter();
+            return true;
+          },
+          Backspace: ({ editor }) => {
+            if (editor.isEmpty) {
+              handleDestroyOnEmpty(id);
+            }
+            return false;
+          },
+        };
+      },
+    });
 
-  // Editor
-  const editor = useEditor({
-    extensions: [
-      ShortcutsExtension,
-      StarterKit.configure({
-        history: false,
-      }),
-      Highlight,
-      Underline,
-      Placeholder.configure({
-        placeholder: ({ node }) => {
-          if (node.type.name === "heading") {
-            return `Heading ${node.attrs.level}`;
-          }
-          return "Press ‘space’ for AI, ‘/’ for commands…";
-        },
-      }),
-      Heading.configure({
-        levels: [1, 2, 3],
-      }),
-    ],
-    autofocus: true,
-  });
+    // Editor
+    const editor = useEditor({
+      extensions: [
+        ShortcutsExtension,
+        StarterKit.configure({
+          history: false,
+        }),
+        Highlight,
+        Underline,
+        Placeholder.configure({
+          placeholder: ({ node }) => {
+            if (node.type.name === "heading") {
+              return `Heading ${node.attrs.level}`;
+            }
+            return "Press ‘space’ for AI, ‘/’ for commands…";
+          },
+        }),
+        Heading.configure({
+          levels: [1, 2, 3],
+        }),
+      ],
+      autofocus: true,
+    });
 
-  // Store
-  const dispatch = useDispatch();
+    // Store
+    const dispatch = useDispatch();
 
-  // Handles
-  const handleKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
-      // onChange();
-    }
-  };
-  const handleEnter = () => {
-    dispatch(newBlock());
-  };
-  const handleDestroyOnEmpty = (id: string) => {
-    dispatch(destroyBlock({ id }));
-  };
+    // Handles
+    // const handleKeyDown = (e: React.KeyboardEvent) => {
+    //   if (e.key === "Enter") {
+    //     // onChange();
+    //   }
+    // };
+    const handleEnter = () => {
+      dispatch(newBlock());
+    };
+    const handleDestroyOnEmpty = (id: string) => {
+      onDestroy();
+      dispatch(destroyBlock({ id }));
+    };
 
-  return (
-    <div className={"text-block"}>
-      {editor && (
-        <BubbleMenu
+    useImperativeHandle(ref, () => editor, [editor]);
+
+    return (
+      <div className={"text-block"}>
+        {editor && (
+          <BubbleMenu
+            editor={editor}
+            tippyOptions={{ duration: 100 }}
+            className="text-action-menu"
+          >
+            <Button
+              style={{ borderRadius: 0, transition: "none" }}
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              className={editor.isActive("bold") ? "is-active" : ""}
+              type="text"
+              icon={<BoldOutlined />}
+            />
+
+            <Button
+              style={{ borderRadius: 0, transition: "none" }}
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              className={editor.isActive("italic") ? "is-active" : ""}
+              type="text"
+              icon={<ItalicOutlined />}
+            />
+
+            <Button
+              style={{ borderRadius: 0, transition: "none" }}
+              onClick={() => editor.chain().focus().toggleUnderline().run()}
+              className={editor.isActive("italic") ? "is-active" : ""}
+              type="text"
+              icon={<UnderlineOutlined />}
+            />
+
+            <Button
+              style={{ borderRadius: 0, transition: "none" }}
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              className={editor.isActive("strike") ? "is-active" : ""}
+              type="text"
+              icon={<StrikethroughOutlined />}
+            />
+
+            <Button
+              style={{ borderRadius: 0, transition: "none" }}
+              onClick={() => editor.chain().focus().toggleHighlight().run()}
+              className={editor.isActive("highlight") ? "is-active" : ""}
+              type="text"
+              icon={<HighlightOutlined />}
+            />
+          </BubbleMenu>
+        )}
+        {editor && (
+          <FloatingMenu
+            editor={editor}
+            tippyOptions={{
+              duration: 100,
+              placement: "bottom-start",
+            }}
+            shouldShow={({ editor }) => {
+              return (
+                editor.getText() === "/"
+                // && editor.getJSON().content![0].type === "paragraph"
+              );
+            }}
+          >
+            <List className="floating-menu-list" split={false}>
+              <List.Item
+                onClick={() => {
+                  editor.chain().focus().clearContent().run();
+                }}
+              >
+                <List.Item.Meta
+                  avatar={<Avatar shape="square" size={46} src={TextImage} />}
+                  title={"Text"}
+                  description={"Just start writing with plain text."}
+                />
+              </List.Item>
+              <List.Item
+                onClick={() =>
+                  editor
+                    .chain()
+                    .focus()
+                    .clearContent()
+                    .toggleHeading({ level: 1 })
+                    .run()
+                }
+              >
+                <List.Item.Meta
+                  avatar={
+                    <Avatar shape="square" size={46} src={Heading1Image} />
+                  }
+                  title={"Heading 1"}
+                  description={"Big section heading."}
+                />
+              </List.Item>
+              <List.Item
+                onClick={() =>
+                  editor
+                    .chain()
+                    .focus()
+                    .clearContent()
+                    .toggleHeading({ level: 2 })
+                    .run()
+                }
+              >
+                <List.Item.Meta
+                  avatar={
+                    <Avatar shape="square" size={46} src={Heading2Image} />
+                  }
+                  title={"Heading 2"}
+                  description={"Medium section heading."}
+                />
+              </List.Item>
+              <List.Item
+                onClick={() =>
+                  editor
+                    .chain()
+                    .focus()
+                    .clearContent()
+                    .toggleHeading({ level: 3 })
+                    .run()
+                }
+              >
+                <List.Item.Meta
+                  avatar={
+                    <Avatar shape="square" size={46} src={Heading3Image} />
+                  }
+                  title={"Heading 3"}
+                  description={"Small section heading."}
+                />
+              </List.Item>
+            </List>
+          </FloatingMenu>
+        )}
+        <EditorContent
+          content={content}
           editor={editor}
-          tippyOptions={{ duration: 100 }}
-          className="text-action-menu"
-        >
-          <Button
-            style={{ borderRadius: 0, transition: "none" }}
-            onClick={() => editor.chain().focus().toggleBold().run()}
-            className={editor.isActive("bold") ? "is-active" : ""}
-            type="text"
-            icon={<BoldOutlined />}
-          />
-
-          <Button
-            style={{ borderRadius: 0, transition: "none" }}
-            onClick={() => editor.chain().focus().toggleItalic().run()}
-            className={editor.isActive("italic") ? "is-active" : ""}
-            type="text"
-            icon={<ItalicOutlined />}
-          />
-
-          <Button
-            style={{ borderRadius: 0, transition: "none" }}
-            onClick={() => editor.chain().focus().toggleUnderline().run()}
-            className={editor.isActive("italic") ? "is-active" : ""}
-            type="text"
-            icon={<UnderlineOutlined />}
-          />
-
-          <Button
-            style={{ borderRadius: 0, transition: "none" }}
-            onClick={() => editor.chain().focus().toggleStrike().run()}
-            className={editor.isActive("strike") ? "is-active" : ""}
-            type="text"
-            icon={<StrikethroughOutlined />}
-          />
-
-          <Button
-            style={{ borderRadius: 0, transition: "none" }}
-            onClick={() => editor.chain().focus().toggleHighlight().run()}
-            className={editor.isActive("highlight") ? "is-active" : ""}
-            type="text"
-            icon={<HighlightOutlined />}
-          />
-        </BubbleMenu>
-      )}
-      {editor && (
-        <FloatingMenu
-          editor={editor}
-          tippyOptions={{
-            duration: 100,
-            placement: "bottom-start",
+          onKeyDown={(event) => {
+            // handleKeyDown();
+            onArrowPressed?.(event);
           }}
-          shouldShow={({ editor }) => {
-            return (
-              editor.getText() === "/"
-              // && editor.getJSON().content![0].type === "paragraph"
-            );
-          }}
-        >
-          <List className="floating-menu-list" split={false}>
-            <List.Item
-              onClick={() => {
-                editor.chain().focus().clearContent().run();
-              }}
-            >
-              <List.Item.Meta
-                avatar={<Avatar shape="square" size={46} src={TextImage} />}
-                title={"Text"}
-                description={"Just start writing with plain text."}
-              />
-            </List.Item>
-            <List.Item
-              onClick={() =>
-                editor
-                  .chain()
-                  .focus()
-                  .clearContent()
-                  .toggleHeading({ level: 1 })
-                  .run()
-              }
-            >
-              <List.Item.Meta
-                avatar={<Avatar shape="square" size={46} src={Heading1Image} />}
-                title={"Heading 1"}
-                description={"Big section heading."}
-              />
-            </List.Item>
-            <List.Item
-              onClick={() =>
-                editor
-                  .chain()
-                  .focus()
-                  .clearContent()
-                  .toggleHeading({ level: 2 })
-                  .run()
-              }
-            >
-              <List.Item.Meta
-                avatar={<Avatar shape="square" size={46} src={Heading2Image} />}
-                title={"Heading 2"}
-                description={"Medium section heading."}
-              />
-            </List.Item>
-            <List.Item
-              onClick={() =>
-                editor
-                  .chain()
-                  .focus()
-                  .clearContent()
-                  .toggleHeading({ level: 3 })
-                  .run()
-              }
-            >
-              <List.Item.Meta
-                avatar={<Avatar shape="square" size={46} src={Heading3Image} />}
-                title={"Heading 3"}
-                description={"Small section heading."}
-              />
-            </List.Item>
-          </List>
-        </FloatingMenu>
-      )}
-      <EditorContent
-        content={content}
-        editor={editor}
-        onKeyDown={handleKeyDown}
-      />
-    </div>
-  );
-};
+        />
+      </div>
+    );
+  }
+);
 
 export default TextBlock;
--- frontend/src/components/Page/Page.tsx
+++ frontend/src/components/Page/Page.tsx
@@ -1,25 +1,33 @@
 import "./Page.css";
-import React from "react";
+import React, { useRef } from "react";
 import BaseBlock from "../Block/Block";
 import TextBlock from "../Block/components/TextBlock/TextBlock";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { Block } from "../../store/slices/blocksSlice";
+import { Editor } from "@tiptap/react";
 
 const Page: React.FC = () => {
+  // Refs
+  const refs = useRef<(Editor | null)[]>([]);
+
   // Store
   const blocks: Block[] = useSelector(
     (state: RootState) => state.blocksReducer.blocks
   );
 
   // Handles
-  const handleCreateNewBlock = () => {
-    console.log("Hello");
+  // const handleCreateNewBlock = () => {
+  //   console.log("Hello");
+  // };
+
+  const handleFocus = (index: number) => {
+    refs.current[index]?.chain().focus().run();
   };
 
   return (
     <div style={{ maxWidth: "100%", minWidth: 0, width: "900px" }}>
-      {blocks.map((block) => (
+      {blocks.map((block, index) => (
         <BaseBlock
           key={block.id}
           children={
@@ -27,7 +35,19 @@ const Page: React.FC = () => {
               id={block.id}
               content={block.content}
               type={block.type}
-              onChange={handleCreateNewBlock}
+              // onChange={handleCreateNewBlock}
+              ref={(ref) => {
+                refs.current[index] = ref;
+              }}
+              onArrowPressed={(event) => {
+                const direction: Record<string, number> = {
+                  ArrowUp: -1,
+                  ArrowDown: 1,
+                };
+                const nextIndex: number = index + direction[event.key];
+                handleFocus(nextIndex);
+              }}
+              onDestroy={() => handleFocus(index - 1)}
             />
           }
         />
