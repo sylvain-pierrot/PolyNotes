c2627e11715fdfb1b87ba22fdf79808a60b18b99
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2023-03-27 15:16:28 +0200

    refactor(helm): remove useless files

--- .helm/backend/src/mails/mails.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common';
-import { MailsService } from './mails.service';
-
-@Controller('email')
-export class MailsController {
-  constructor(private readonly mailsService: MailsService) {}
-}
