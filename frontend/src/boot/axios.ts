24b0ca39c8c306c2918df9ae5a691242d67944d1
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2023-04-01 20:15:10 +0200

    refact(frontend): use .env to provide base_url api

--- frontend/src/boot/axios.ts
+++ frontend/src/boot/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
   headers: {
-    "Access-Control-Allow-Origin": "http://localhost:5000",
+    "Access-Control-Allow-Origin": process.env.REACT_APP_BASE_URL,
     "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS, PATCH",
   },
 });
