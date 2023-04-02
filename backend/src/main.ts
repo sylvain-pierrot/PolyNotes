e11c5f32bf3a36a01be57ae26ca809df2529144a
sylvain-pierrot <sylvain.pierrot@etu.umontpellier.fr>
2023-04-02 18:22:14 +0200

    fix(backend): cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,10 @@ async function bootstrap() {
   app.enableCors({
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    origin: 'https://polynotes.cluster-2022-9.dopolytech.fr',
+    origin: [
+      'https://polynotes.cluster-2022-9.dopolytech.fr',
+      'http://localhost:3000',
+    ],
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
